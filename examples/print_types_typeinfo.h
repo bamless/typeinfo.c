3b7936c96416cbb6c545b629189b848626cab1eb
Fabrizio Pietrucci <mail@fabrizio-pietrucci.it>
2026-02-08 21:44:05 +0100

    Remove test binary

--- examples/print_types_typeinfo.h
+++ examples/print_types_typeinfo.h
@@ -20,13 +20,13 @@ extern Type_Info_Float typeinfo_float;
 extern Type_Info_Float typeinfo_double;
 extern Type_Info_Float typeinfo_long_double;
 
-extern Type_Info_Struct typeinfo_Foo; // /home/fabrizio/Workspace/c/typeinfo.c/examples/print_types.h:24:9
-extern Type_Info_Enum typeinfo_Color; // /home/fabrizio/Workspace/c/typeinfo.c/examples/print_types.h:30:9 
-extern Type_Info_Union typeinfo_TestUnion; // /home/fabrizio/Workspace/c/typeinfo.c/examples/print_types.h:39:9
-extern Type_Info_Struct typeinfo_TestAnonymousEnum; // /home/fabrizio/Workspace/c/typeinfo.c/examples/print_types.h:49:9
-extern Type_Info_Struct typeinfo_TestUnnamedAnonymous; // /home/fabrizio/Workspace/c/typeinfo.c/examples/print_types.h:54:9
-extern Type_Info_Struct typeinfo_TestQualifiers; // /home/fabrizio/Workspace/c/typeinfo.c/examples/print_types.h:65:9
-extern Type_Info_Struct typeinfo_Bar; // /home/fabrizio/Workspace/c/typeinfo.c/examples/print_types.h:13:9
-extern Type_Info_Struct typeinfo_Baz; // /home/fabrizio/Workspace/c/typeinfo.c/examples/print_types.h:8:9
+extern Type_Info_Struct typeinfo_Foo; // examples/print_types.h:24:9
+extern Type_Info_Enum typeinfo_Color; // examples/print_types.h:30:9 
+extern Type_Info_Union typeinfo_TestUnion; // examples/print_types.h:39:9
+extern Type_Info_Struct typeinfo_TestAnonymousEnum; // examples/print_types.h:49:9
+extern Type_Info_Struct typeinfo_TestUnnamedAnonymous; // examples/print_types.h:54:9
+extern Type_Info_Struct typeinfo_TestQualifiers; // examples/print_types.h:65:9
+extern Type_Info_Struct typeinfo_Bar; // examples/print_types.h:13:9
+extern Type_Info_Struct typeinfo_Baz; // examples/print_types.h:8:9
 
 #endif // PRINT_TYPES_TYPEINFO_H_
